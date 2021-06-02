225172dd0a0987fd717b02a332a8a18cd7507470
niunanmian <1229033303@qq.com>
2021-06-02 22:42:20 +0800

    practise php

---
+++ 123.php
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE html>
+<html>
+<body>
+
+<h1>我的第一张 PHP 页面</h1>
+
+<?php
+
+//如果需要设置允许所有域名发起的跨域请求，可以使用通配符 *
+header("Access-Control-Allow-Origin: *");
+
+echo  file_get_contents("http://www.weather.com.cn/data/cityinfo/101220701.html");
+?>
+
+</body>
+</html>
---
+++ cityAndPro.js
@@ -0,0 +1,9 @@
+var cityMessage = [
+    { "regid": "2", "parid": "1", "regname": "北京", "regtype": "1", "ageid": "0" },
+    { "regid": "3", "parid": "1", "regname": "安徽", "regtype": "1", "ageid": "0"},
+    //市
+    { "regid": "36", "parid": "3", "regname": "合肥", "regtype": "2", "ageid": "0","code":"101220101" },
+    { "regid": "38", "parid": "3", "regname": "安庆", "regtype": "2", "ageid": "0","code":"101220601" },
+    { "regid": "37", "parid": "2", "regname": "北京", "regtype": "3", "ageid": "0","code":"101010100" },]
+
+
